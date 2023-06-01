7b6b38eca78894019aab14f325e60a156b13062d
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-06-01 15:39:18 +0700

    add docker compose web dock

---
+++ web_dock/server/server.js
@@ -0,0 +1,15 @@
+// server/server.js
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8080, function () {
+  console.log("App listening on port 8080!");
+});
